5395a78ae31c792ee29185905654001362697c9b
Bastian Schwarz <bastian@codename-php.de>
2021-08-18 00:20:14 +0200

    Remove ruby-shadow again since it is broken on ruby 3.0

--- test/fixtures/cookbooks/test/recipes/default.rb
+++ test/fixtures/cookbooks/test/recipes/default.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 
-# oh well ... https://github.com/apalmblad/ruby-shadow/issues/26
-# chef_gem 'ruby-shadow' do # purge the gem since it is not included in all installs so we need to test the install in the resource
-#  action :purge
-# end
-
 codenamephp_users_from_data_bag 'Create users' do
   groups %w(chef sudo sysadmin)
 end
